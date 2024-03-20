e6a7de655eb2e0eef857af40b3810ad991bd0e02
Emil Fauzan <emilfauzan@gmail.com>
2024-03-20 10:50:55 +0700

    using div as parent does not solve the problem

--- components/navigation/Topbar/Topbar.tsx
+++ components/navigation/Topbar/Topbar.tsx
@@ -12,6 +12,7 @@ import {
 const Topbar = () => {
     return (
         <>
+        <div>
             <div>
                 <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
                     <div className="flex items-center justify-between text-blue-gray-900">
@@ -120,6 +121,7 @@ const Topbar = () => {
                     the next time.
                 </Typography>
             </div>
+        </div>
         </>
     )
 }
