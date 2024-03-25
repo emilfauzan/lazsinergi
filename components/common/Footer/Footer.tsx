11208834c93590748366a67d3ac4ad39c60e52f2
Emil Fauzan <emilfauzan@gmail.com>
2024-03-25 08:43:57 +0700

    added section

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <>
+            <div className='grid grid-cols-2 bg-green-100/50 mt-8 py-4'>
+                <h2 className='my-4 mx-6'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci enim a amet modi doloremque repellendus distinctio sit fugiat obcaecati debitis.</h2>
+                <h2 className='my-4 mx-6'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci enim a amet modi doloremque repellendus distinctio sit fugiat obcaecati debitis.</h2>
+            </div>
+            <div className='bg-blue-100/50 pt-8 pb-2 text-center'>
+                 <h2>Copyright, Lorem ipsum.</h2>
+            </div>
+        </>
+    )
 }
 
 export default Footer
