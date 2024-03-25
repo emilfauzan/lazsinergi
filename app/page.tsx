2ebb812be17b879656b8d8029229970a089e941b
Emil Fauzan <emilfauzan@gmail.com>
2024-03-25 08:33:48 +0700

    modified home page with each layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Footer from '@/components/common/Footer/Footer';
+import Topbar from '@/components/navigation/Topbar/Topbar';
 import Home from '@/pages/dashboard/home/Home'
 
 export default function Dashboard() {
   return (
     <main className="overflow-hidden">
+      <Topbar/>
       <Home />
+      <Footer/>
 
     </main>
   );
