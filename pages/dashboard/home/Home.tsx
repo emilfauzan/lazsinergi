5d095075c6ce27aae282b4332e8e6d2298b9287d
Emil Fauzan <emilfauzan@gmail.com>
2024-04-04 09:37:12 +0700

    aded carousel with grid & modified sponsors

--- pages/dashboard/home/Home.tsx
+++ pages/dashboard/home/Home.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Typography } from '@material-tailwind/react'
+import { Carousel, Typography } from '@material-tailwind/react'
 import { Button } from '@mui/material'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import LAZSlogonametagline5 from "@/public/images/companyLogo/LAZSlogonametagline5.png";
@@ -117,9 +117,46 @@ function Home() {
             </div>
 
             {/* Mitra */}
-            <h2 className='my-4 text-center'>Lorem, ipsum.</h2>
+            <Typography variant='lead' className='my-4 text-center' placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Mitra Kami</Typography>
             <div className='grid grid-cols-1 pb-4'>
                 <h2 className='py-4 text-center bg-[#F1F5F9]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci enim a amet modi doloremque repellendus distinctio sit fugiat obcaecati debitis.</h2>
+                <Carousel
+                    className="rounded-xl" loop
+                    navigation={({ setActiveIndex, activeIndex, length }) => (
+                        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+                            {new Array(length).fill("").map((_, i) => (
+                                <span
+                                    key={i}
+                                    className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"}`}
+                                    onClick={() => setActiveIndex(i)} />
+                            ))}
+                        </div>
+                    )} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                >
+                    
+                    <div className='grid grid-cols-2 gap-3 my-4 mx-6 items-center'>
+                        <img
+                            src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+                            alt="image 1"
+                        />
+                        <img
+                            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+                            alt="image 2"
+                        />
+                    </div>
+                    <div className='grid grid-cols-2 gap-3 my-4 mx-6 items center'>
+                        <img
+                            src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+                            alt="image 3"
+                            className="h-full w-full object-cover"
+                        />
+                        <img
+                            src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+                            alt="image 3"
+                            className="h-full w-full object-cover"
+                        />
+                    </div>
+                    
+                </Carousel>
             </div>
 
             {/* Warning */}
